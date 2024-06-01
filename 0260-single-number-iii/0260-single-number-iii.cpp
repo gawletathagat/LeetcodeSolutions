528f9cdc7ffec4619c1e3e0a0f25c80321bd3867
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2024-06-01 20:27:29 +0530

    Time: 6 ms (68.76%), Space: 12.3 MB (64.88%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -4,30 +4,32 @@ public:
     {
         // sc(n), tc(n) // map
         // sc(1), tc(n^2) // for loop
-        // bitmanupulation
-        int overall_xor = 0;
+        // bitmanupulation //  //striver
+        long overall_xor = 0;
         for (int n : nums) {
             overall_xor ^= n;
         }
 
         int first_group_xor = 0;
         int second_group_xor = 0;
-
-        int bit_pos_dif = 0;
-        while (((overall_xor >> bit_pos_dif) & 1) != 1) {
-            bit_pos_dif++;
-        }
+        
+        //striver
+        int bit_pos_dif = (overall_xor & (overall_xor-1)) ^ overall_xor;
+        
        
 
-        for (int num : nums) {
-            cout<<((num >> bit_pos_dif)& 1) <<" nums ";
-            if (((num >> bit_pos_dif) & 1) == 1) {
-                first_group_xor ^= num;
-                cout<<first_group_xor<<" first_group_xor "<<endl;
-            } else {
-                second_group_xor ^= num;
-                cout<<second_group_xor<<" second_group_xor "<<endl;
-            }
+        for (int i= 0 ; i<nums.size() ; i++) {
+            // cout<<((num >> bit_pos_dif)& 1) <<" nums ";
+            // if (((num >> bit_pos_dif) & 1) == 1) {
+            //     first_group_xor ^= num;
+            //     cout<<first_group_xor<<" first_group_xor "<<endl;
+            // } else {
+            //     second_group_xor ^= num;
+            //     cout<<second_group_xor<<" second_group_xor "<<endl;
+            // }
+            if( nums[i] & bit_pos_dif )  first_group_xor ^= nums[i];
+            else second_group_xor ^= nums[i];
+
         }
 
         return {first_group_xor, second_group_xor};
