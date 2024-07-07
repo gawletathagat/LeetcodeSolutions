ff785a858b334cb3236862b68ea5d31b39a65414
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2024-07-07 21:14:33 +0530

    Time: 0 ms (100%), Space: 6.9 MB (95.58%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int res = numBottles;
-        int cur_empty = numBottles;
+        int totalBottles = numBottles;
 
-        while (cur_empty / numExchange != 0) {
-            int full_drinked = cur_empty / numExchange;
-            int left_empty = cur_empty - full_drinked * numExchange;
-            res += full_drinked;
-            cur_empty = left_empty + full_drinked;
+        while (numBottles >= numExchange) {
+            int newBottles = numBottles / numExchange;
+            int remainingBottles = numBottles % numExchange;
+            totalBottles += newBottles;
+            numBottles = newBottles + remainingBottles;
         }
 
-        return res;
+        return totalBottles;
     }
 };
