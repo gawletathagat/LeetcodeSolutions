5eef3fc25e6e4f7d060ddd9ae410595666ac4f91
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2024-07-23 09:10:49 +0530

    Time: 15 ms (7.67%), Space: 15.1 MB (25.59%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -1,47 +1,16 @@
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
-
-        unordered_map<int,int> freq;
-
-        for(auto &num: nums){
-
-            freq[num]++;
-            
+        unordered_map<int, int> frequencyMap;
+        for (int n : nums) {
+            frequencyMap[n]++;
         }
-
-        map<int,vector<int>> freqGrp;
-
-        for(auto &it: freq){
-
-            freqGrp[it.second].push_back(it.first);
-            
-        }
-
-        vector<int> ans;
-
-        for(auto &it: freqGrp){
-
-            if(it.second.size() > 1){
-
-                sort(it.second.rbegin(), it.second.rend());
-                
+        sort(nums.begin(), nums.end(), [&](int a, int b) {
+            if (frequencyMap[a] != frequencyMap[b]) {
+                return frequencyMap[a] < frequencyMap[b];
             }
-
-            for(auto &num: it.second){
-
-                for(int i=0;i<it.first;i++){
-
-                    ans.push_back(num);
-                    
-                }
-                
-            }
-            
-        }
-
-        return ans;
-        
-        
+            return a > b;
+        });
+        return nums;
     }
 };
